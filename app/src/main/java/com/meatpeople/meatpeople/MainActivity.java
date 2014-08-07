c93b60ba5e8f49372c4b05dfaf159d9d023e74cf
mschroer <mschroer15@gmail.com>
2014-08-07 15:12:08 -0400

    working Parse/Facebook login. First pass at backend api

---
+++ app/src/main/java/com/meatpeople/meatpeople/MainActivity.java
@@ -0,0 +1,78 @@
+package com.meatpeople.meatpeople;
+
+import android.app.Activity;
+import android.app.Application;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.meatpeople.meatpeople.R;
+
+public class MainActivity extends Fragment {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public static class ErrorDialogFragment extends DialogFragment {
+        private Dialog mDialog;
+
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
+
+    private boolean servicesConnected() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (ConnectionResult.SUCCESS == resultCode) {
+            return true;
+        } else {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
+            if (dialog != null) {
+                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+                errorFragment.setDialog(dialog);
+                errorFragment.show(getSupportFragmentManager(), Application.TAG);
+            }
+            return false;
+        }
+    }
+}
--- app/src/main/java/com/meatpeople/meatpeople/MeatPeopleApplication.java
+++ app/src/main/java/com/meatpeople/meatpeople/MeatPeopleApplication.java
@@ -13,30 +13,12 @@ import java.text.ParseException;
  * Created by Matt on 8/6/2014.
  */
 public class MeatPeopleApplication extends Application {
-
+    public static String TAG = "MeatPeople";
     @Override
     public void onCreate() {
         super.onCreate();
         Parse.initialize(this, "dqUWztAqdE4HmRBkfeWeguyvoqIRIp7Lt8eUJTHc", "6ZxfKEpXUVoOg2AZyVRWIegyoz1zuEleP9xvI3sZ");
         ParseFacebookUtils.initialize("668927893188791");
-        ParseUser user = new ParseUser();
-        user.setUsername("my name");
-        user.setPassword("my pass");
-        user.setEmail("email@example.com");
-
-        // other fields can be set just like with ParseObject
-        user.put("phone", "650-555-0000");
-
-        user.signUpInBackground(new SignUpCallback() {
-            public void done(com.parse.ParseException e) {
-                if (e == null) {
-                    // Hooray! Let them use the app now.
-                } else {
-                    // Sign up didn't succeed. Look at the ParseException
-                    // to figure out what went wrong
-                }
-            }
-        });
     }
 }
 
